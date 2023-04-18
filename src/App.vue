6b46d68c64b557610febfbb0305798743c682016
zhouqiyun <1468453978@qq.com>
2023-04-18 14:05:48 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"/>  
+    <!--$route代表当前路由  -->
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  export default {
+    components:{
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="less">
+  .app {
+    width: 100%;
+    height: 100%;
+    background-color: #f5f5f5;
+  }
+</style>
